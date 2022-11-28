559682462407a666fc3a8cf6ae06daa045ad4130
Valeryi Dnkovtsev <valorapaulinio@gmail.com>
2022-11-28 14:11:21 +0300

    main change

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         int price = 10_000;
         int miles = service.calculate(price);
         System.out.println(miles);
+        System.out.println(price / miles);
     }
 }
