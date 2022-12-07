a8b36bc1f486b931ba40e479aad163e02bd6df2e
Valeryi Dnkovtsev <valorapaulinio@gmail.com>
2022-12-07 16:33:09 +0300

    change metod

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,15 +1,8 @@
 public class BonusMilesService {
 
     public int calculate(int cost) {
-        cost = 20;
-        int price = 10_000;
-        int result;
 
-
-        result = price / cost;
-
-
-        return result;
+        return cost / 20;
 
 
     }
